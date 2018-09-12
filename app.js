90d7e33af5a8ed4a78f654b229ef4ff918f90101
Rino Vetha Moses J <36627034+rinovethamoses97@users.noreply.github.com>
2018-09-12 11:32:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var bodyparser=require('body-parser');
 var app=express();
 var route=require('./route/route');
 app.use('/api',route);
-
-
-app.listen(3000,function(){
-	console.log("Server running in port:3000");
-});
+var http = require("http");
+var server = http.createServer(app);
+var port = Number(process.env.PORT || 3000);
+server.listen(port);
