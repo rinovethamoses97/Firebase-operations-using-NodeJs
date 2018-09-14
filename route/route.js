f1cfe65381deb83777ef7061814529a536ec2bc5
rino <rinovethamoses97@gmail.com>
2018-09-14 18:58:13 +0530

    path changes

--- route/route.js
+++ route/route.js
@@ -1,25 +1,22 @@
 var express=require('express');
 var bodyparser=require('body-parser');
 var cors=require('cors');
-var admin = require("firebase-admin");
-
 var router=express.Router();
 router.use(cors());
 router.use(bodyparser.json());
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://breatheeasy-7c7af.firebaseio.com"
 });
-router.get('/insert/:name',function(req,res){
+router.get('/insert/:d1/:d2',function(req,res){
 	var db = admin.database();
-	var ref=db.ref();
+	var ref=db.ref('ard01/');
 	ref.set({
-		name:req.params.name,
+		d1:req.params.d1,
+		d2:req.params.d2,
 	})
-	res.send("success");
+	res.send("success"+req.params.d1+req.params.d2);
 });
 module.exports=router;
