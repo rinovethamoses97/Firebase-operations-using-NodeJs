9271bd7ecb190e21ca1200c0b28ffbe04e645136
rino <rinovethamoses97@gmail.com>
2018-09-14 19:14:33 +0530

    Bug fixing:bug:

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var bodyparser=require('body-parser');
 var app=express();
 var route=require('./route/route');
 app.use('/api',route);
-var http = require("http");
-var server = http.createServer(app);
-var port = Number(process.env.PORT || 3000);
-server.listen(port);
+// var http = require("http");
+// var server = http.createServer(app);
+// var port = Number(process.env.PORT || 3000);
+// server.listen(port);
+app.listen(3000,function(){
+	console.log("Server running in port 3000"+ new Date());
+});
