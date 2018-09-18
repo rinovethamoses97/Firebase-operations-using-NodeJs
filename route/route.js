1209b71479f9199c7f7de2d86cf2903d9ac2f5a9
M.S.Vignesh <msv99999@gmail.com>
2018-09-18 14:27:35 +0530

    Update route.js

--- route/route.js
+++ route/route.js
@@ -14,7 +14,7 @@ router.get('/insert/:d1/:d2/:d3',function(req,res){
 	var db = admin.database();
 	var ref=db.ref('ard01/test');
 	ref.update({
-		req.params.d1:req.params.d2,
+		d1:req.params.d2,
 		d1:req.params.d3,
 		
 	})
