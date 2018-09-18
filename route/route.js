b73faba491561293c4a46f4da60ef22819c0bee5
M.S.Vignesh <msv99999@gmail.com>
2018-09-18 14:22:48 +0530

    Update route.js

--- route/route.js
+++ route/route.js
@@ -10,13 +10,14 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://breatheeasy-7c7af.firebaseio.com"
 });
-router.get('/insert/:d1/:d2',function(req,res){
+router.get('/insert/:d1/:d2/:d3',function(req,res){
 	var db = admin.database();
 	var ref=db.ref('ard01/test');
 	ref.update({
-		d1:req.params.d1,
-		d2:req.params.d2,
+		req.params.d1:req.params.d2,
+		req.params.d1:req.params.d3,
+		
 	})
-	res.send("success"+req.params.d1+req.params.d2);
+	res.send("success"+req.params.d1+req.params.d2+req.params.d3);
 });
 module.exports=router;
